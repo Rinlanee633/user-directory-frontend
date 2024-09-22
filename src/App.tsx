6f84c5227d414fecf2947877f3b980b46ac32d56
Rinlanee Watcharanimmankul <166051815+Rinlanee633@users.noreply.github.com>
2024-09-22 08:54:14 +0700

    Refactor to use AuthContext.tsx instead of useAuth.ts for login and implement error handling on the login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-import { useAuth } from './hooks/useAuth';
 import Login from './pages/Login';
 import Home from './pages/Home';
 
-const App = () => {
-  const { isLoggedIn } = useAuth();
+import { AuthProvider } from './contexts/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
 
+
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route
-            path = "/home"
-            element = { isLoggedIn ? <Home /> : <Navigate to="/login" replace={true} />}
-        />
-        {/* <Route path = "/login" element = { isLoggedIn ? <Navigate to="/home" replace={true} /> : <Login />} /> */}
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        {/* <Route path="/home" element={<Home />} /> */}
-      
-      </Routes>
-    </Router>
+    <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/home"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/" element={<Navigate to="/login" />} />
+          </Routes>
+        </Router>
+    </AuthProvider>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useAuth } from '../contexts/AuthContext';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-};
 
+  const { logout, user } = useAuth();
+  
+  return (
+    <div style={{ padding: '20px' }}>
+    <h1>Welcome to the Home Page!</h1>
+    {user && <h2>Hello, {user.username}!</h2>}
+    <button onClick={logout}>Logout</button>
+  </div>
+  );
+};
 export default Home;

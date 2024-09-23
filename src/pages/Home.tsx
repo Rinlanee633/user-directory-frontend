af8608b8502d9aba4206778888c0682bfa039870
Rinlanee Watcharanimmankul <166051815+Rinlanee633@users.noreply.github.com>
2024-09-23 13:01:38 +0700

    Update: Show welcome message after login using AuthProvider

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,57 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import GlobalStyles from '../styles.ts/globalStyles';
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 import Sidebar from '../components/Sidebar/Sidebar';
 import Header from '../components/Header'
 import UserTable from '../components/tables/UserTable';
+import { useAuth } from '../contexts/AuthContext';
+
+
+const slideIn = keyframes`
+  from {
+    transform: translateX(-100%);
+    opacity: 0;
+  }
+  to {
+    transfrom: traslatex(0);
+    opacity: 1;
+    
+`;
 
 const Container = styled.div`
   margin-left: 250px;
   margin-right: 100px;
+  align-items: center;
+`;
+
+const Message = styled.h1<{slide: boolean}>`
+  animation: ${props => props.slide ? slideIn : 'none'} 3s forwards;
+  position: absolute;
+  margin-left: 10px;
+  color: #212121;
+
 `;
 
 const Home = () => {
+  const { isLoggedIn } = useAuth();
+  const [showWelcome, setShowWelcome] = useState(false);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      setShowWelcome(true);
+    }
+  }, [isLoggedIn]); 
+
   return (
     <>
     <GlobalStyles />
     <Sidebar />
     <Header />
     <Container>
-        <h1 style = {{marginLeft:'10px', color: '#212121'}}>Welcome!</h1>
-        <UserTable />
+      <div style={{position: 'relative', height: '60px'}}>
+          {showWelcome && <Message slide={showWelcome}>Welcome!</Message>}
+      </div>
+      <UserTable />
     </Container>
     
   </>

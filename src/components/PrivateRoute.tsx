916ba4d5aaa0435b1c75cfc959ccab29e9bab206
Rinlanee Watcharanimmankul <166051815+Rinlanee633@users.noreply.github.com>
2024-09-27 13:53:48 +0700

    Update Private Route logic to avoid redirecting to login on refresh

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 interface PrivateRouteProps {
   children: React.ReactNode;
@@ -8,6 +8,10 @@ interface PrivateRouteProps {
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
   const { isLoggedIn } = useAuth();
+
+  if (isLoggedIn == null) {
+    return null;
+  }
 
   return isLoggedIn ? <>{children}</> : <Navigate to="/login" />;
 };
--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,8 +1,14 @@
-import React, { createContext, useContext, useEffect, useState, ReactNode} from 'react';
-import { User, users } from '../mocks/loginUserData';
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import { User, users } from "../mocks/loginUserData";
 
 interface AuthContextType {
-  isLoggedIn: boolean;
+  isLoggedIn: boolean | null;
   login: (username: string, password: string) => boolean;
   logout: () => void;
   error: string | null;
@@ -16,62 +22,66 @@ interface AuthProviderProps {
   children: ReactNode;
 }
 
-
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [user, setUser] = useState<{ username: string } | null>(null);
 
   useEffect(() => {
-    const loggedIn = localStorage.getItem('isLoggedIn');
-    const storedUser = localStorage.getItem('user');
-    if (loggedIn === 'true' && storedUser) {
+    const loggedIn = localStorage.getItem("isLoggedIn");
+    const storedUser = localStorage.getItem("user");
+    if (loggedIn === "true" && storedUser) {
       setIsLoggedIn(true);
       setUser(JSON.parse(storedUser));
+    } else {
+      setIsLoggedIn(false);
     }
   }, []);
 
   const login = (username: string, password: string): boolean => {
-    console.log('Login called with', username, password);
+    console.log("Login called with", username, password);
 
-        if (!username || !password) {
-            setError('Please enter your username and password.');
-            return false;
-        }
+    if (!username || !password) {
+      setError("Please enter your username and password.");
+      return false;
+    }
 
-        try {
-            const foundUser = users.find(
-                (currentUser: User) => currentUser.username === username && currentUser.password === password
-            );
-           
-            if (foundUser) {
-                const userData = { username };
-                localStorage.setItem('isLoggedIn', 'true')
-                localStorage.setItem('user', JSON.stringify(userData));
+    try {
+      const foundUser = users.find(
+        (currentUser: User) =>
+          currentUser.username === username && currentUser.password === password
+      );
 
-                setIsLoggedIn(true);
-                setUser({username});
-                setError(null);
-                return true;
-            } else {
-                setError('Username or password is incorrect.');
-                return false;
-            }
-        } catch (error) {
-            setError('Login failed. Please try again.');
-            return false;
-        }
+      if (foundUser) {
+        const userData = { username };
+        localStorage.setItem("isLoggedIn", "true");
+        localStorage.setItem("user", JSON.stringify(userData));
+
+        setIsLoggedIn(true);
+        setUser({ username });
+        setError(null);
+        return true;
+      } else {
+        setError("Username or password is incorrect.");
+        return false;
+      }
+    } catch (error) {
+      setError("Login failed. Please try again.");
+      return false;
+    }
   };
 
   const logout = () => {
-        localStorage.removeItem('isLoggedIn')
-        localStorage.removeItem('user');
-        setIsLoggedIn(false);
-        setUser(null);
+    localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("user");
+    setIsLoggedIn(false);
+    setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout, error, setError, user}}>
+    <AuthContext.Provider
+      value={{ isLoggedIn, login, logout, error, setError, user }}
+    >
       {children}
     </AuthContext.Provider>
   );
@@ -80,7 +90,7 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error("useAuth must be used within an AuthProvider");
   }
   return context;
 };

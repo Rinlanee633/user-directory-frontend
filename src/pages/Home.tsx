2de469d994e12bf0330503f8b26cb1c415c72197
Rinlanee Watcharanimmankul <166051815+Rinlanee633@users.noreply.github.com>
2024-10-09 16:19:50 +0700

    Resolve warning: renamed 'slide' prop to '$slide'

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import GlobalStyles from '../styles.ts/globalStyles';
-import styled, {keyframes} from 'styled-components';
-import Sidebar from '../components/Sidebar/Sidebar';
-import Header from '../components/Header'
-import UserTable from '../components/tables/UserTable';
-import { useAuth } from '../contexts/AuthContext';
-
+import React, { useEffect, useState } from "react";
+import GlobalStyles from "../styles.ts/globalStyles";
+import styled, { keyframes } from "styled-components";
+import Sidebar from "../components/Sidebar/Sidebar";
+import Header from "../components/Header";
+import UserTable from "../components/tables/UserTable";
+import { useAuth } from "../contexts/AuthContext";
 
 const slideIn = keyframes`
   from {
@@ -24,12 +23,11 @@ const Container = styled.div`
   align-items: center;
 `;
 
-const Message = styled.h1<{slide: boolean}>`
-  animation: ${props => props.slide ? slideIn : 'none'} 3s forwards;
+const Message = styled.h1<{ $slide: boolean }>`
+  animation: ${(props) => (props.$slide ? slideIn : "none")} 3s forwards;
   position: absolute;
   margin-left: 10px;
   color: #212121;
-
 `;
 
 const Home = () => {
@@ -37,25 +35,23 @@ const Home = () => {
   const [showWelcome, setShowWelcome] = useState(false);
 
   useEffect(() => {
-    if (isLoggedIn) {
+    if (isLoggedIn && !showWelcome) {
       setShowWelcome(true);
     }
-  }, [isLoggedIn]); 
+  }, [isLoggedIn, showWelcome]);
 
   return (
     <>
-    <GlobalStyles />
-    <Sidebar />
-    <Header />
-    <Container>
-      <div style={{position: 'relative', height: '60px'}}>
-          {showWelcome && <Message slide={showWelcome}>Welcome!</Message>}
-      </div>
-      <UserTable />
-    </Container>
-    
-  </>
-  
+      <GlobalStyles />
+      <Sidebar />
+      <Header />
+      <Container>
+        <div style={{ position: "relative", height: "60px" }}>
+          {showWelcome && <Message $slide={showWelcome}>Welcome!</Message>}
+        </div>
+        <UserTable />
+      </Container>
+    </>
   );
 };
 export default Home;
